cc240d3f1c9369aa3c0cb8a94c40b5c6d8758077
Sean Hazlett <seanpat13@gmail.com>
2023-03-24 11:34:46 +0000

    completing binary tree

--- src/generics/BinaryTree.java
+++ src/generics/BinaryTree.java
@@ -1,5 +1,38 @@
 package generics;
 
 public class BinaryTree {
+	TreeNode<Integer> root;
+	
+	
+	public void PrintInOrderTraversal()
+	{
+		
+	}
 
+	public void InsertNode(Integer newVal)
+	{
+		if(root==null) root.val = newVal;
+		
+		else
+		{
+			TreeNode <Integer> cur = root;
+			while(true)
+			{
+				if(newVal>=cur.val && cur.right == null)
+				{
+					cur.right = new TreeNode<Integer>(newVal);
+					break;
+				}
+				else if(newVal<cur.val && cur.left == null) 
+				{
+					cur.left = new TreeNode<Integer>(newVal);
+					break;
+				}
+				else
+				{
+					cur = (newVal>=cur.val) ? cur.right :  cur.left;
+				}
+			}
+		}
+	}
 }
